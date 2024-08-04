403f9a0e12b717a7bf8511e2a75e404df6843689
David <crunchyforwatch@gmail.com>
2024-08-04 17:30:47 -0400

    Got name.co, working to some degree.

--- public_html/motorcycle/Features.php
+++ public_html/motorcycle/Features.php
@@ -1,32 +1,20 @@
+
+
 <?php
-
-
-$curl = curl_init();
-
-curl_setopt_array($curl, [
-    CURLOPT_URL => "https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=imperial&mode=xml",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => [
-        "x-rapidapi-host: community-open-weather-map.p.rapidapi.com",
-        "x-rapidapi-key: Sign Up for Key"
-    ],
-]);
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-    echo "cURL Error #:" . $err;
-} else {
-    echo $response;
-}
+$variable = "hello"
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<h1>My First <? $variable ?></h1>
+<p>My first paragraph.</p>
+
+
+
+</body>
+</html>
 
 
