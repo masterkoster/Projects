5e81e2a1c52399a642b694eda119dfd3e57fc2ec
David <crunchyforwatch@gmail.com>
2024-08-04 02:06:22 -0400

    have been trying to get the server to work and folders and got it running.. haven't even started programming, been more IT if anything else.

--- motorcycles/index.php
+++ motorcycles/index.php
@@ -1,14 +1,17 @@
+
+
+// Link to domain https://www.determined-haibt.157-245-90-72.plesk.page/motorcycles/index.php
+
 <?php
 ?>
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+</head>
 <body>
-
-<
-<h1>testing git second time motorcyle </h1>
-
-
-
+<h1>Hello, world!</h1>
 </body>
 </html>
-
