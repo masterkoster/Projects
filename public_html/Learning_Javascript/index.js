e942a5fc711ccfbf82f23cca16835656f73dfb2d
masterkoster <37987259+masterkoster@users.noreply.github.com>
2024-08-06 11:24:54 -0400

    Dylan recommended me not to do PHP and focus on javascript first.. I will take his advice to heart

---
+++ public_html/Learning_Javascript/index.js
@@ -0,0 +1,6 @@
+const name = 1
+console.log("test" + name + "test")
+
+const Brands = ['Suzuki', 'Honda', 'BMW']
+
+console.log(Brands)
---
+++ public_html/Learning_Javascript/main.js
@@ -0,0 +1,33 @@
+//test
+
+const name = 1
+console.log("test " + name + " test")
+
+const Brands = ['Suzuki', 'Honda', 'BMW']
+Brands[5] = 'Harley'
+
+Brands.push('Aprilla')
+Brands.unshift('Yamaha')
+console.log(Brands)
+console.log(Brands[2])
+
+const person = {
+    firstName:'johnn',
+    lastName: 'doe' ,
+    hobbies: ['riding', 'snowboarding', 'flying'],
+    address: {
+        street:'Blokmakersstraat',
+        city: 'Rotterdam',
+        state:'South Holland'
+    }
+}
+
+console.log(person);
+console.log(person.hobbies[2])
+console.log(person.address.city)
+console.log(person.address.state)
+
+const { firstName, lastName, address: {city}} = person;
+console.log(city);
+person.email = 'david@koster.im';
+console.log(person.email);
